c435d81cb9109bf033a6370c406f6771b7ea3c3f
MAYCON GIBSON NEVES DOS SANTOS <maycon_g_santos@estudante.sesisenai.org.br>
2025-06-17 20:13:14 -0300

    '.'

--- Sa/src/main.jsx
+++ Sa/src/main.jsx
@@ -4,13 +4,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from 'react-router-dom';
 import router from './commom/service/router';
-// Selecione o elemento raiz
+
 const rootElement = document.getElementById("root");
 
-// Crie a raiz com "createRoot"
 const root = ReactDOM.createRoot(rootElement);
 
-// Renderize o componente
 root.render(
     <React.StrictMode>
         <RouterProvider router={router} />
